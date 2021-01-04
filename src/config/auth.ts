e04365a3bf673815bf49f73b0754cdf1b0801db8
lstaborda <leticia.s.taborda@gmail.com>
2021-01-03 21:20:01 -0300

    Initial commit

---
+++ src/config/auth.ts
@@ -0,0 +1,6 @@
+export default {
+  jwt: {
+    secret: '5d34c464ff8d0334e327cb4bd12914b9',
+    expiresIn: '1d',
+  },
+};
---
+++ src/routes/users.routes.ts
@@ -0,0 +1,47 @@
+import { Router } from 'express';
+import multer from 'multer';
+
+import uploadConfig from '../config/upload';
+
+import CreateUserService from '../services/CreateUserService';
+import ensureAuthenticated from '../middlewares/ensureAuthenticated';
+
+const usersRouter = Router();
+const upload = multer(uploadConfig);
+
+usersRouter.post('/', async (request, response) => {
+  try {
+    const { name, email, password } = request.body;
+
+    const createUser = new CreateUserService();
+
+    const user = await createUser.execute({
+      name,
+      email,
+      password,
+    });
+
+    const userWithoutPassword = {
+      id: user.id,
+      name: user.name,
+      email: user.email,
+      created_at: user.created_at,
+      updated_at: user.updated_at,
+    };
+
+    return response.json(userWithoutPassword);
+  } catch (error) {
+    return response.status(400).json({ error: error.message });
+  }
+});
+
+usersRouter.patch(
+  '/avatar',
+  ensureAuthenticated,
+  upload.single('avatar'),
+  async (request, response) => {
+    return response.json({});
+  },
+);
+
+export default usersRouter;
